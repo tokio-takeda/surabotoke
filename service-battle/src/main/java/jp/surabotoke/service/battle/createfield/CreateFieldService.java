a8eb4c051ee6fb0d4a9f3c61e2059d1db36f4923
tokio-takeda <defeated.diabolical.pitch@gmail.com>
2016-02-11 23:29:46 +0900

    戦闘サービスを追加
    戦場作成サービスを作成

---
+++ service-battle/src/main/java/jp/surabotoke/service/battle/createfield/CreateFieldService.java
@@ -0,0 +1,7 @@
+package jp.surabotoke.service.battle.createfield;
+
+import jp.surabotoke.model.battlefield.BattleField;
+
+public interface CreateFieldService {
+	public BattleField createField(String avatarName);
+}
---
+++ service-battle/src/main/java/jp/surabotoke/service/battle/createfield/CreateOneToOneFieldService.java
@@ -0,0 +1,14 @@
+package jp.surabotoke.service.battle.createfield;
+
+import jp.surabotoke.model.battlefield.BattleField;
+import jp.surabotoke.model.battlefield.OneToOneFactory;
+
+public class CreateOneToOneFieldService implements CreateFieldService {
+
+	@Override
+	public BattleField createField(String avatarName) {
+		OneToOneFactory oneToOneFactory = OneToOneFactory.getInstance();
+		return oneToOneFactory.create(avatarName);
+	}
+	
+}
